330ceb4a0f55bf254203e5f247ca8eb559261274
Kushal <kushal0213@gmail.com>
2025-08-02 01:56:48 +0530

    update

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lyra AI Assistant",
+  description: "Lyra is a smart and intuitive AI voice assistant designed to help you get things done effortlessly through natural conversation.",
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="h-screen overflow-hidden">{children}</body>
+    </html>
+  );
+}
